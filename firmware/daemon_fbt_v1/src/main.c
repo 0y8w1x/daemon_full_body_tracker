5e8033d600b5d4197a20ce271662fc438ec5191a
0y8w1x <joseph.albers@outlook.de>
2024-04-25 17:32:42 +0200

    successfully run modified ESB example on custom nrf52805 board

--- firmware/daemon_fbt_v1/src/main.c
+++ firmware/daemon_fbt_v1/src/main.c
@@ -1,6 +1,135 @@
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/nrf_clock_control.h>
+#include <zephyr/irq.h>
+#include <nrf.h>
+#include <esb.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/kernel.h>
+#include <zephyr/types.h>
 
-int main(void)
+static bool ready = true;
+static struct esb_payload rx_payload;
+static struct esb_payload tx_payload = ESB_CREATE_PAYLOAD(0,
+	0x01, 0x00, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08);
+
+#define _RADIO_SHORTS_COMMON                                                   \
+	(RADIO_SHORTS_READY_START_Msk | RADIO_SHORTS_END_DISABLE_Msk |         \
+	 RADIO_SHORTS_ADDRESS_RSSISTART_Msk |                                  \
+	 RADIO_SHORTS_DISABLED_RSSISTOP_Msk)
+
+void event_handler(struct esb_evt const *event) {
+	ready = true;
+	switch (event->evt_id) {
+	case ESB_EVENT_TX_SUCCESS:
+		break;
+	case ESB_EVENT_TX_FAILED:
+		break;
+	case ESB_EVENT_RX_RECEIVED:
+	        while (esb_read_rx_payload(&rx_payload) == 0);
+		break;
+	}
+}
+int clocks_start(void)
 {
+	int err;
+	int res;
+	struct onoff_manager *clk_mgr;
+	struct onoff_client clk_cli;
+
+	clk_mgr = z_nrf_clock_control_get_onoff(CLOCK_CONTROL_NRF_SUBSYS_HF);
+	if (!clk_mgr) {
+                // unable to get the clock manager
+		return -ENXIO;
+	}
+	sys_notify_init_spinwait(&clk_cli.notify);
+	err = onoff_request(clk_mgr, &clk_cli);
+	if (err < 0) {
+		return err;
+	}
+
+	do {
+	        err = sys_notify_fetch_result(&clk_cli.notify, &res);
+		if (!err && res) {
+                        // clock could not be started
+			return res;
+		}
+	} while (err);
+
+	return 0;
+}
+
+int esb_initialize(void)
+{
+	int err;
+	/* These are arbitrary default addresses. In end user products
+	 * different addresses should be used for each set of devices.
+	 */
+	uint8_t base_addr_0[4] = {0xE7, 0xE7, 0xE7, 0xE7};
+	uint8_t base_addr_1[4] = {0xC2, 0xC2, 0xC2, 0xC2};
+	uint8_t addr_prefix[8] = {0xE7, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8};
+
+	struct esb_config config = ESB_DEFAULT_CONFIG;
+
+	config.protocol = ESB_PROTOCOL_ESB_DPL;
+	config.retransmit_delay = 600;
+	config.bitrate = ESB_BITRATE_2MBPS;
+	config.event_handler = event_handler;
+	config.mode = ESB_MODE_PTX;
+	config.selective_auto_ack = true;
+
+	err = esb_init(&config);
+
+	if (err) {
+		return err;
+	}
+
+	err = esb_set_base_address_0(base_addr_0);
+	if (err) {
+		return err;
+	}
+
+	err = esb_set_base_address_1(base_addr_1);
+	if (err) {
+		return err;
+	}
+
+	err = esb_set_prefixes(addr_prefix, ARRAY_SIZE(addr_prefix));
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+int main(void) {
+        int err;
+
+        err = clocks_start();
+	if (err) {
+		return 0;
+	}
+
+        err = esb_initialize();
+	if (err) {
+                // esb initialization failed
+		return 0;
+	}
+
+        tx_payload.noack = false;
+	while (1) {
+		if (ready) {
+			ready = false;
+			esb_flush_tx();
+			// update(tx_payload.data[1]);
+
+			err = esb_write_payload(&tx_payload);
+			if (err) {
+                                // payload write failed
+			}
+			tx_payload.data[1]++;
+		}
+		k_sleep(K_MSEC(100));
+	}
         return 0;
 }
